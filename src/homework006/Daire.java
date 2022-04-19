706822b39ac407567e8aa2d566f02c6ab134967c
BİLAL <bllsfk.3984@gmail.com>
2022-04-20 02:27:24 +0300

    HW006 has been completed by Bilal Şafak

---
+++ src/homework006/Daire.java
@@ -0,0 +1,17 @@
+package homework006;
+
+public class Daire {
+    private double yaricap;
+
+    public Daire(double yaricap) {
+        this.yaricap = yaricap < 0 ? 0 : yaricap;
+    }
+
+    public double getYaricap() {
+        return yaricap;
+    }
+
+    public double getAlan() {
+        return (yaricap * yaricap * Math.PI);
+    }
+}
