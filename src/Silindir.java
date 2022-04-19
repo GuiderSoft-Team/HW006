844716cf7093cd8d5086b078753d7a5b8ee18714
AlkanTuncer <alkantuncer53@hotmail.com>
2022-04-19 18:30:11 +0300

    Alkan Tuncer HW006

---
+++ src/Silindir.java
@@ -0,0 +1,23 @@
+public class Silindir extends Daire{
+
+
+    private double yukseklik;
+
+    public Silindir(double yaricap,double yukseklik) {
+        super(yaricap);
+        this.yukseklik=yukseklik <= 0 ? 0 : yukseklik;
+    }
+
+    public double getYukseklik() {
+        return yukseklik;
+    }
+
+    public double getHacim(){
+        return super.getAlan()*yukseklik;
+    }
+
+    @Override
+    public double getAlan() {
+        return (2*getYaricap()*getYaricap()*Math.PI) + (2* Math.PI*getYaricap()*yukseklik);
+    }
+}
