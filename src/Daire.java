91f6df4b0acd54cebfac4a04bc5994df8fc83e4f
NurannNalci <nuran.nalann2411@gmail.com>
2022-06-09 18:46:33 +0200

    HW006

--- src/Daire.java
+++ src/Daire.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class daire {
+public class Daire {
+
+    private double yaricap;
+
+
+    public Daire(double yaricap) {
+        setYaricap(yaricap);
+    }
+    public void setYaricap(double yaricap) {
+        if(yaricap<0){
+            yaricap=0;
+        }
+        this.yaricap=yaricap;
+    }
+
+    public double getYaricap() {
+
+        return yaricap;
+    }
+
+    public double getAlan(){
+
+        return yaricap*yaricap*Math.PI;
+    }
+
+    public void showInfo(){
+        System.out.println("Daire...");
+        Daire daire = new Daire(getYaricap());
+        System.out.printf("Yarıçap=%.2f\n" ,daire.getYaricap());
+        System.out.printf("Alan= %.2f\n",daire.getAlan());
+        System.out.println("");
+    }
+
 }
--- src/Silindir.java
+++ src/Silindir.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Silindir {
-}
+public class Silindir extends Daire {
+
+        private double yukseklik;
+
+        public Silindir(double yaricap ,double yukseklik) {
+            super(yaricap);
+            setYukseklik(yukseklik);
+        }
+
+        public double getYukseklik() {
+            return yukseklik;
+        }
+
+        public void setYukseklik(double yukseklik) {
+            if(yukseklik<0){
+                yukseklik=0;
+            }
+            this.yukseklik = yukseklik;
+        }
+
+        @Override
+        public double getAlan() {
+            return (2*getYaricap()*getYaricap()*Math.PI)+(2*Math.PI*getYaricap()*yukseklik);
+        }
+
+        public double getHacim(){
+            return super.getAlan()*yukseklik;
+        }
+
+        public void showInfo(){
+
+
+            Silindir silindir = new Silindir(getYaricap(),getYukseklik());
+            System.out.println("Silindir...");
+            System.out.printf("Yarıçap=%.2f\n", silindir.getYaricap());
+            System.out.printf("Yükseklik=%.2f\n", silindir.getYukseklik());
+            System.out.printf("Alan=%.2f\n" , silindir.getAlan());
+            System.out.printf("Hacim=%.2f", silindir.getHacim());
+
+        }
+    }
+
